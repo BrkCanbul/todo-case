d579aa4fcd8033eb2fdb504b4ff4cfff1c7812b8
Burak Canbul <burakcanbul41@gmail.com>
2025-05-06 23:46:25 +0000

    refactor: reorganize main function to improve service and controller initialization

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"todo-case/routes"
-
+	"todo-case/services"
+	"todo-case/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default();
+	r := gin.Default()
 
-	routes.Setup(r)
+	todoService := services.NewMockTodoService()
+	userService := services.NewMockUserService()
+
+	todoController := controllers.NewTodoController(todoService)
+	authController := controllers.NewAuthController(userService)
+
+	routes.SetupRoutes(r, todoController, authController)
+
 	r.Run(":8080")
 }
