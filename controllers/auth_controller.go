1b927609f9a220508dd156acb5cf8b7d3ca126af
Burak Canbul <burakcanbul41@gmail.com>
2025-05-06 23:46:38 +0000

    feat: implement AuthController with login functionality and refactor TodoController methods

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,26 +1,45 @@
+// controllers/auth_controller.go
 package controllers
 
 import (
 	"net/http"
 	"todo-case/utils"
+	"todo-case/services"
 
 	"github.com/gin-gonic/gin"
 )
 
-type login struct {
-	UserId uint `json:"user_id"`
+type AuthController struct {
+	UserService services.UserService
 }
 
-func Login(c *gin.Context) {
-	var input login
-	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+func NewAuthController(userService services.UserService) *AuthController {
+	return &AuthController{UserService: userService}
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (ctl *AuthController) Login(c *gin.Context) {
+	var req LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Veriler geçersiz"})
 		return
 	}
-	token, err := utils.GenerateKey(input.UserId)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "couldn't create token", "details": err.Error()})
-	}
-	c.JSON(http.StatusOK, gin.H{"token": token})
 
+	user, ok := ctl.UserService.ValidateCredentials(req.Username, req.Password)
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Kullanıcı adı veya şifre hatalı"})
+		return
+	}
+
+	token, err := utils.GenerateKey(user.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Token oluşturulamadı"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
 }
--- controllers/crud_controller.go
+++ controllers/crud_controller.go
@@ -3,34 +3,51 @@ package controllers
 import (
 	"net/http"
 	"todo-case/models"
+	"todo-case/services"
+
 	"github.com/gin-gonic/gin"
 )
 
-
-func GetLists(c *gin.Context){
-	
-	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+type TodoController struct {
+	srv services.TodoService
 }
 
-func AddTodoCase(c *gin.Context){
-
-	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
-}
-func RemoveTodoCase(c *gin.Context){
-	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
-}
-func AddToDoList(c *gin.Context){
-	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
-}
-
-func RemoveFromList(c *gin.Context){
-	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
-}
-func EditTodo(c *gin.Context){
-	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+func NewTodoController(service services.TodoService) *TodoController {
+	return &TodoController{srv: service}
 
 }
-func EditList(c *gin.Context){
-	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+func (ctl *TodoController) GetLists(c *gin.Context) {
+	todos := ctl.srv.GetAll()
+	c.JSON(http.StatusOK, todos)
+
+}
+
+func AddTodoCase(c *gin.Context) {
+
+	c.JSON(http.StatusNotImplemented, gin.H{"error": "this method is not implemented"})
+}
+func RemoveTodoCase(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{"error": "this method is not implemented"})
+}
+func (ctl *TodoController) AddToDoList(c *gin.Context) {
+	var todolist models.ToDoList
+	if err := c.ShouldBindJSON(&todolist); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+	}
+	created := ctl.srv.Create(&todolist)
+
+	c.JSON(http.StatusOK, created)
+}
+
+func RemoveFromList(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{"error": "this method is not implemented"})
+}
+func EditTodo(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{"error": "this method is not implemented"})
+
+}
+func EditList(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{"error": "this method is not implemented"})
 
 }
