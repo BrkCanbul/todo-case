69fde658f5ca0a0bdf67468add114c9c4081f608
Burak Canbul <burakcanbul41@gmail.com>
2025-05-06 08:32:34 +0000

    feat: add CRUD controller with placeholder methods

---
+++ controllers/crud_controller.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"net/http"
+	"todo-case/models"
+	"github.com/gin-gonic/gin"
+)
+
+
+func GetLists(c *gin.Context){
+	
+	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+}
+
+func AddTodoCase(c *gin.Context){
+
+	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+}
+func RemoveTodoCase(c *gin.Context){
+	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+}
+func AddToDoList(c *gin.Context){
+	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+}
+
+func RemoveFromList(c *gin.Context){
+	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+}
+func EditTodo(c *gin.Context){
+	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+
+}
+func EditList(c *gin.Context){
+	c.JSON(http.StatusNotImplemented,gin.H{"error":"this method is not implemented"})
+
+}
