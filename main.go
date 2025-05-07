787e6448c4ca572290111b1044610b9717bd5739
Burak Canbul <burakcanbul41@gmail.com>
2025-05-07 01:14:30 +0000

    feat: add Swagger documentation and setup route for API documentation

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
+// @title Todo API
+// @version 1.0
+// @description Görev yönetimi servisi
+// @host localhost:8080
+// @BasePath /
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description JWT formatında bearer token giriniz. Örn: "Bearer {token}"
+
+
 package main
+
 
 import (
 	"todo-case/routes"
 	"todo-case/services"
 	"todo-case/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/files"
+	_ "todo-case/docs"
+
 )
 
 func main() {
@@ -17,6 +33,6 @@ func main() {
 	authController := controllers.NewAuthController(userService)
 
 	routes.SetupRoutes(r, todoController, authController)
-
+	r.GET("/swagger/*any",ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run(":8080")
 }
